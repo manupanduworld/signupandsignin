ce7fe2206877599396fc5133b07112dc6324befc
Manu Pandu <manu@gmail.com>
2020-04-10 19:01:35 +0530

    second changes

--- authentication/authenticationapp/models.py
+++ authentication/authenticationapp/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    User = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
     date_of_birth = models.DateField()
     phone_no = models.CharField(max_length=15)
     address = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.User.username
+        return self.user.username
--- authentication/authenticationapp/views.py
+++ authentication/authenticationapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from .forms import *
 
+
 # Create your views here.
 def signup(request):
     form = SignUpForm()
@@ -9,12 +10,18 @@ def signup(request):
 
     if request.method == 'POST':
         user_form = SignUpForm(request.POST)
-        profile_form = SignUpForm(request.POST)
+        profile_form = ProfileForm(request.POST)
         if user_form.is_valid() and profile_form.is_valid():
-            form.save()
+            new_user = user_form.save()
+            # create profile for the user
+            Profile.objects.create(user=new_user)
+            profile_form = ProfileForm(instance=new_user.profile, data=request.POST)
+            profile_form.save()
 
-    context = {'form':form}
+    context = {'user_form': user_form, "profile_form": profile_form}
     return render(request, 'authenticationapp/signup.html', context)
+
+
 def signin(request):
     context = {}
     return render(request, 'authenticationapp/signin.html', context)
